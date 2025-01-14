f3801ce4cb9bc1ef5307009a3cebad1bfd73dce6
HoriaMoraru <94983746+HoriaMoraru@users.noreply.github.com>
2025-01-14 18:29:10 +0200

    Added files for homework 3.

--- checker/checker.sh
+++ checker/checker.sh
@@ -6,7 +6,7 @@ setup()
 {
     cp -r --no-clobber $SRC_PATH/* .
     make build
-    make tester
+    make compiler
 }
 
 cleanup()
@@ -16,7 +16,29 @@ cleanup()
 
 run_tests()
 {
-    make run
+    CLASSPATH=.:$(realpath ../antlr-4.13.0-complete.jar)
+
+    passed=0
+    for source_file in ./tests/*.cl; do
+        echo -e "\n`basename $source_file`"
+        java -cp $CLASSPATH cool.compiler.Compiler $source_file > ./tests/`basename $source_file .cl`.s
+        if [ "$source_file" = "./tests/32-big.cl" ]; then
+            echo 5 | spim -exception_file trap.handler.nogc -file tests/`basename $source_file .cl`.s > tests/`basename $source_file .cl`.out
+        else
+            spim -exception_file trap.handler.nogc -file tests/`basename $source_file .cl`.s > tests/`basename $source_file .cl`.out
+        fi
+
+        diff tests/`basename $source_file .cl`.ref tests/`basename $source_file .cl`.out
+
+        if [ $? = 0 ]; then
+            echo -e "Test passed!\n"
+            passed=$(($passed + 1))
+        else
+            echo -e "Test failed!\n"
+        fi
+    done
+
+    echo -e "\nTotal: $(( $(($passed * 100)) / 32))"
 }
 
 setup
